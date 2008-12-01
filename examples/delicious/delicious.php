89a9fd39436f5f8820a597a25b27bb0ae7077213
Stefan Marsiske <stefan.marsiske@gmail.com>
2008-12-01 22:02:46 +0100

    [enh] added error handling to demo

--- examples/delicious/delicious.php
+++ examples/delicious/delicious.php
@@ -15,34 +15,38 @@ if(isset($_GET['end'])) {
 $days=array();
 $oDelicious = new PhpDelicious($_GET['user'], $_GET['pass']);
 $aPosts = $oDelicious->GetAllPosts();
-if ($aPosts) {
-   foreach ($aPosts as $aPost) {
-      list($date,$time)=explode(" ",$aPost['updated']);
-      if(!isset($days[$date])) {
-         $days[$date]=array('count'=>1, 'tags'=>array());
-      } else {
-         $days[$date]['count']+=1;
-      }
-      foreach($aPost['tags'] as $tag) {
-         if(!isset($days[$date][$tag])) {
-            $days[$date]['tags'][$tag]=1;
+if(!$oDelicious->LastErrorNo()==0) {
+   print json_encode(array("error" => $oDelicious->LastErrorString()."please wait 6min before retrying."));
+} else {
+   if ($aPosts) {
+      foreach ($aPosts as $aPost) {
+         list($date,$time)=explode(" ",$aPost['updated']);
+         if(!isset($days[$date])) {
+            $days[$date]=array('count'=>1, 'tags'=>array());
          } else {
-            $days[$date]['tags'][$tag]+=1;
+            $days[$date]['count']+=1;
+         }
+         foreach($aPost['tags'] as $tag) {
+            if(!isset($days[$date][$tag])) {
+               $days[$date]['tags'][$tag]=1;
+            } else {
+               $days[$date]['tags'][$tag]+=1;
+            }
          }
       }
-   }
-   $result=array();
-   ksort($days);
-   foreach($days as $day => $opt) {
-      $tags=$opt['tags'];
-      $tmp=array();
-      foreach(array_keys($tags) as $tag) {
-         $tmp[]=array($tag,$tags[$tag]);
+      $result=array();
+      ksort($days);
+      foreach($days as $day => $opt) {
+         $tags=$opt['tags'];
+         $tmp=array();
+         foreach(array_keys($tags) as $tag) {
+            $tmp[]=array($tag,$tags[$tag]);
+         }
+         $result[]=array($day,$tmp);
       }
-      $result[]=array($day,$tmp);
+      print json_encode($result);
+   } else {
+      print_r($aPosts);
    }
-   print json_encode($result);
-} else {
-   echo $oDelicious->LastErrorString();
 }
 ?>
